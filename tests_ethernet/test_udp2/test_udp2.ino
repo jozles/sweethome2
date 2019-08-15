ebf86b0ccefcd479f5f168189be3ed2bafa09f92
jozles <pinkasfeld@combox.fr>
2019-08-15 13:58:39 +0200

    fonctionne

--- tests_ethernet/test_udp2/test_udp2.ino
+++ tests_ethernet/test_udp2/test_udp2.ino
@@ -1,23 +1,12 @@
-/*
-  UDPSendReceive.pde:
- This sketch receives UDP message strings, prints them to the serial port
- and sends an "acknowledge" string back to the sender
- A Processing sketch is included at the end of file that can be used to send
- and received messages for testing with a computer.
- created 21 Aug 2010
- by Michael Margolis
- This code is in the public domain.
- */
 
-
-#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <SPI.h>        
 #include <Ethernet.h>
-#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+#include <EthernetUdp.h>
 
-IPAddress masterIp(192, 168, 1, 30);
+IPAddress masterIp(192, 168, 0, 30);
 unsigned int masterPort = 8887;
 
-#define SLAVE
+//#define SLAVE
 
 #ifndef SLAVE
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
@@ -27,7 +16,7 @@ byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
 #ifdef SLAVE
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE};
-IPAddress slaveIp(192, 168, 1, 31);
+IPAddress slaveIp(192, 168, 0, 31);
 #define IP slaveIp
 #define PORT 8888
 #endif
@@ -36,13 +25,85 @@ IPAddress slaveIp(192, 168, 1, 31);
 
 EthernetUDP Udp;
 
-int i;
+int i,cnt=0;
 
-char* chexa="0123456789ABCDEFabcdef\0";
+void setup() {
+  
+  Serial.begin(115200);
 
+// show mac,ip,port of current machine on serial
+  for(i=0;i<6;i++){Serial.print(mac[i],HEX);if(i!=5){Serial.print(":");}}
+  Serial.print(" ready on ");
+  for(i=0;i<4;i++){Serial.print(IP[i]);if(i!=3){Serial.print(".");}}
+  Serial.print("  ");
+  Serial.println(PORT);
 
-void conv_htoa(char* ascii,byte* h)
+// start ethernet,udp  
+  Ethernet.begin(mac, IP);
+  if(!Udp.begin(PORT)){Serial.println("Udp.begin ko");while(1){};}
+
+}
+
+// recieve an udp message
+int rxUdp(IPAddress* ipAddr,unsigned int* rxPort,char* data)
 {
+  int packetSize = Udp.parsePacket();
+ 
+  if (packetSize){
+    *ipAddr = (uint32_t) Udp.remoteIP();
+    Serial.print("Received packet of size ");Serial.println(packetSize);
+    Serial.print("From ");Serial.print(*ipAddr);Serial.print(" ");
+    
+    *rxPort = (unsigned int) Udp.remotePort();
+    Serial.print(", port ");Serial.println((int)*rxPort);
+
+    Udp.read(data, packetSize);
+    Serial.println("Contents:");Serial.println(data);
+  }
+  
+  return packetSize;
+}
+
+// transmit an udp message
+void txUdp(IPAddress ipAddr,unsigned int port,char* data)
+{
+  Udp.beginPacket(ipAddr,port);
+  
+  Serial.print("sending (");Serial.print(strlen(data));Serial.print(")>");Serial.print(data);
+  Serial.print("< to ");Serial.print(ipAddr);Serial.print(":");Serial.println(port);
+  
+    Udp.write(data,strlen(data));
+    Udp.endPacket();
+}
+
+void loop() 
+{
+  
+  IPAddress rxIp;
+  unsigned int rxPort;
+  char data[MAX_LENGTH];
+  memset(data,0x00,MAX_LENGTH);
+
+#ifdef SLAVE
+  txUdp(masterIp,masterPort,"hello master !");
+  Serial.println("tx");
+  while(rxUdp(&rxIp,&rxPort,data)==0){}
+  cnt++;
+  Serial.println(cnt);delay(1000);
+#endif
+
+#ifndef SLAVE
+  while(rxUdp(&rxIp,&rxPort,data)==0){}
+  txUdp(rxIp,8888,"hi slave!");
+#endif
+
+}
+
+
+ void conv_htoa(char* ascii,byte* h)
+{
+  char* chexa="0123456789ABCDEFabcdef\0";
+  
     uint8_t c=*h,d=c>>4,e=c&0x0f;
     ascii[0]=chexa[d];ascii[1]=chexa[e];
 }
@@ -74,103 +135,3 @@ void dumpfield(char* fd,uint8_t ll)
     for(int ff=ll-1;ff>=0;ff--){if((fd[ff]&0xF0)==0){Serial.print("0");}Serial.print(fd[ff],HEX);}
     Serial.print(" ");
 }
-
-
-
-
-void setup() {
-
-  
-  Serial.begin(115200);
-
-  Serial.println("ready ");
-
-  for(i=0;i<6;i++){Serial.print(mac[i],HEX);if(i!=5){Serial.print(":");}}
-  Serial.print("  ");
-  for(i=0;i<4;i++){Serial.print(IP[i]);if(i!=3){Serial.print(".");}}
-  Serial.print("  ");
-  Serial.println(PORT);
-  Ethernet.begin(mac, IP);
-  Udp.begin(PORT);
-
-}
-
-int rxUdp(IPAddress* ipAddr,unsigned int* rxPort,char* data)
-{
-  // if there's data available, read a packet
-  int packetSize = Udp.parsePacket();
-  if (packetSize){
-    Serial.print("Received packet of size ");Serial.println(packetSize);
-    Serial.print("From ");
-    *ipAddr = Udp.remoteIP();
-    IPAddress remote = Udp.remoteIP();
-    for (int i=0; i<4; i++){Serial.print(remote[i], DEC);if(i<3){Serial.print(".");}}
-    *rxPort = Udp.remotePort();
-    Serial.print(", port ");Serial.println(*rxPort);
-
-    // read the packet
-    Udp.read(data, MAX_LENGTH);
-    Serial.println("Contents:");Serial.println(data);
-  }
-  return packetSize;
-}
-
-void txUdp(IPAddress ipAddr,unsigned int port,char* data)
-{
-  dumpstr((char*)&ipAddr,16);
-    Udp.beginPacket(ipAddr,port);
-  Serial.println("tx0");
-    Udp.write(data);
-  Serial.println("tx1");    
-    Udp.endPacket();
-}
-
-void loop() 
-{
-  
-  IPAddress rxIp;
-  unsigned int rxPort;
-  char data[MAX_LENGTH];
-
-#ifdef SLAVE
-  txUdp(masterIp,masterPort,"hello master !");
-  Serial.println("tx");
-  while(rxUdp(&rxIp,&rxPort,data)==0){}
-  while(1){}
-#endif
-
-#ifndef SLAVE
-  while(rxUdp(&rxIp,&rxPort,data)==0){}
-  txUdp(rxIp,rxPort,"hi slave!");
-#endif
-
-}
-
-
-/*
-  Processing sketch to run with this example
- =====================================================
- // Processing UDP example to send and receive string data from Arduino
- // press any key to send the "Hello Arduino" message
- import hypermedia.net.*;
- UDP udp;  // define the UDP object
- void setup() {
- udp = new UDP( this, 6000 );  // create a new datagram connection on port 6000
- //udp.log( true );     // <-- printout the connection activity
- udp.listen( true );           // and wait for incoming message
- }
- void draw()
- {
- }
- void keyPressed() {
- String ip       = "192.168.1.177"; // the remote IP address
- int port        = 8888;    // the destination port
- udp.send("Hello World", ip, port );   // the message to send
- }
- void receive( byte[] data ) {      // <-- default handler
- //void receive( byte[] data, String ip, int port ) { // <-- extended handler
- for(int i=0; i < data.length; i++)
- print(char(data[i]));
- println();
- }
- */
