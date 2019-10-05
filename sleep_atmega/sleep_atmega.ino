6d17516bcfc57fafa13928258203c26702ecbab2
jozles <pinkasfeld@combox.fr>
2019-10-05 23:54:43 +0200

    version de plus en plus "propre" intègre la mise en powerdown d'un circuit nrf ; consommations sous les 4uA en sleep

--- sleep_atmega/sleep_atmega.ino
+++ sleep_atmega/sleep_atmega.ino
@@ -1,36 +1,47 @@
-
+#include <SPI.h>
 #include <avr/power.h>
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 
 /* mécanisme
  *  
- *  la fonction uint16_t sleepPwrDown(durat) gère et effectue la mise en sommeil profond
- *  SLEEP_MODE_PWR_DOWN du MCU, et débranche tout ce qui consomme pour assurer la veille 
- *  la plus sobre possible.
- *  Selon l'argument durat, le réveil provient du watchdog timer ou du pin d'interruption 1 si durat = 0.
+ *  uint16_t sleepPwrDown(durat) deep sleep manager
+ *  make MCU SLEEP_MODE_PWR_DOWN and disable peripheral devices
+ *  
+ *  uint8_t durat is sleep delay by watchdog timer or external awake by INT1 pin if 0.
  *    WDT :
- *    Les durées valides effectuées par le WDT sont définies dans la fonction wdtSetup()
- *    qui initialise le registre WDTCSR pour déclencher le timer.
- *    Au réveil, la fonction wdtDisable() inhibe le WDT. 
- *    La fonction ISR(WDT_vect) est exécutée lors de l'interruption issue du WDT
+ *    valid durations are defined in wdtSetup() wich initialize WDTCSR reg to allow timer start.
+ *    When awaking wdtDisable() disable WDT. 
+ *    ISR(WDT_vect) is WDT vector interrupt service routine 
  *    INT1 :
- *    La fonction int1_ISR() est exécutée lors de l'interruption,
- *    issue du pin INT1
- *    Comme le WDT est stoppé, la consommation est moindre
- *    
- *  la fonction letalSleep() n'effectue pas wdtSetup ni ne branche d'interruption du pin INT1
- *  rien ne sortira le CPU du sommeil sauf un reset physique. (à utiliser si les batteries 
- *  sont épuisées par exemple).
+ *    int1_ISR() for INT1 interrupt
+ *   
+ *   SPI and NRF24L01 access added to test NRF24L01 powerDown mode 
 */
 
+#define NRF
 
-#define LED  4 // 13 test // 4 DETS
-#define REED 3 // 5  test // 3 DETS   (PD3-INT1)
+#define LED  4                  
+#define REED 3                  // reed contact pin (connected to INT1)
+
+#ifdef NRF
+/* SPI and NRF pins */
+#define CLK_PIN    13
+#define MISO_PIN   12
+#define MOSI_PIN   11
+#define CSN_PIN    10
+#define CE_PIN     9
+#endif 
+
+/* voltage pins */
+#define VFACTOR 0.0047          // volts converting 10K+33K
+#define VCHECK  A3              // volts check pin
+#define VINPUT  7               // volts ADC input pin
+
 
 uint16_t wdtTime[]={16,32,64,125,250,500,1000,2000,4000,8000};   // WDT millis
 
-uint8_t nbBlk;                    // blink nb : 1 is from timer, 5 from reed, 1 sec if reed on, 8 sec sleep if reed off
+uint8_t nbBlk;                    // blink nb ... see loop()
 
 ISR(WDT_vect)                     // ISR interrupt service for MPU INT WDT vector
 {
@@ -49,8 +60,18 @@ void blk(uint8_t nbblk)
   pinMode(LED,INPUT); 
 }
 
+float getVolts()                  // read unregulated power voltage (when dedicated resistors mounted... increase power)
+{
+  float volts;
+  analogReference(INTERNAL); 
+  pinMode(VCHECK,OUTPUT);digitalWrite(VCHECK,LOW);
+  volts=analogRead(VINPUT)*VFACTOR;
+  pinMode(VCHECK,INPUT);
+  return volts;
+}
+ 
 
-void wdtSetup(uint8_t durat)  // (0-9) durat>9 for external wdt on INT0 (à traiter)
+void wdtSetup(uint8_t durat)  // (0-9) durat>9 for external wdt on INT0 (not implemented here)
 {
 // datasheet page 51 and 54, Watchdog Timer.
 
@@ -62,7 +83,7 @@ void wdtSetup(uint8_t durat)  // (0-9) durat>9 for external wdt on INT0 (à trai
  *   PORF ------------------ power ON
  *
  *  WDTCSR watchdog timer control
- *   WDIF watchdog interrupt flag (set when int occurs with wdt configured for) (reset byu writing 1 or executing ISR(WDT_vect))
+ *   WDIF watchdog interrupt flag (set when int occurs with wdt configured for) (reset by writing 1 or executing ISR(WDT_vect))
  *   WDIE watchdog interrupt enable  -> counter ovf make interrupt
  *   WDE  watchdog reset enable      -> counter ovf make reset
  *        WDE  WDIE   Mode
@@ -96,23 +117,20 @@ void wdtSetup(uint8_t durat)  // (0-9) durat>9 for external wdt on INT0 (à trai
 #define T4000 0b00100000
 #define T8000 0b00100001
 
-  noInterrupts();
-  wdt_reset();
-
-    MCUSR &= ~(1<<WDRF);  // pour autoriser WDE=0
-
+    noInterrupts();
+    wdt_reset();
+    MCUSR &= ~(1<<WDRF);              // to enable WDE=0
     WDTCSR = (1<<WDCE) | (1<<WDE);    // WDCE ET WDE must be 1
                                       // to write WDP[0-3] and WDE in the following 4 cycles
-    WDTCSR = (1<<WDIE) | durat;       // WDCE must be 0 ; WDE=0, WDIE=1 interrupt mode, TXXX 
-     
-  interrupts();
+    WDTCSR = (1<<WDIE) | durat;       // WDCE must be 0 ; WDE=0, WDIE=1 interrupt mode, TXXX  
+    interrupts();
 }
 
 void wdtDisable()
 {
     noInterrupts();
     wdt_reset();
-    MCUSR &= ~(1<<WDRF);  // pour autoriser WDE=0
+    MCUSR &= ~(1<<WDRF);              // to enable WDE=0
     WDTCSR = (1<<WDCE) | (1<<WDE);    // WDCE ET WDE must be 1 to write WDE in the following 4 cycles
     WDTCSR = 0;                       // WDE et WDIE disabled
     interrupts();                                                    
@@ -151,36 +169,36 @@ uint16_t sleepPwrDown(uint8_t durat)
  *
  * Enabling :
  *
- * power_adc_enable();    // ADC converter
- * power_spi_enable();    // SPI
- * power_usart0_enable(); // Serial (USART)
- * power_timer0_enable(); // Timer 0
- * power_timer1_enable(); // Timer 1
- * power_timer2_enable(); // Timer 2
- * power_twi_enable();    // TWI (I2C)
+ * power_adc_enable();      // ADC converter
+ * power_spi_enable();      // SPI
+ * power_usart0_enable();   // Serial (USART)
+ * power_timer0_enable();   // Timer 0
+ * power_timer1_enable();   // Timer 1
+ * power_timer2_enable();   // Timer 2
+ * power_twi_enable();      // TWI (I2C)
  *
  * Disabling:
  *
- * power_all_disable ();   // turn off all modules
+ * power_all_disable ();    // turn off all modules
  *
- * power_adc_disable();   // ADC converter
- * power_spi_disable();   // SPI
- * power_usart0_disable();// Serial (USART)
- * power_timer0_disable();// Timer 0
- * power_timer1_disable();// Timer 1
- * power_timer2_disable();// Timer 2
- * power_twi_disable();   // TWI (I2C)
+ * power_adc_disable();     // ADC converter
+ * power_spi_disable();     // SPI
+ * power_usart0_disable();  // Serial (USART)
+ * power_timer0_disable();  // Timer 0
+ * power_timer1_disable();  // Timer 1
+ * power_timer2_disable();  // Timer 2
+ * power_twi_disable();     // TWI (I2C)
 */
     ADCSRA &= ~(1<<ADEN);                 // ADC shutdown
     
-    power_all_disable();                  // set all bits in PRR register (modules clk halted)
+    power_all_disable();                  // set PRR register bits (modules clk halted)
     
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);  
 
     if(durat!=0){wdtSetup(durat);}        // setup WDTCSR register for sleep with WDT int awake
     noInterrupts();                       // cli();
 
-    if(durat==0){                         // awake by interrupt
+    if(durat==0){                         // awake by INT1 interrupt
       attachInterrupt(1,int1_ISR,FALLING);
       EIFR=bit(INTF1);                    // clr flag
     }
@@ -188,13 +206,15 @@ uint16_t sleepPwrDown(uint8_t durat)
     sleep_enable();                       
     sleep_bod_disable();                  // BOD halted if sleep_cpu follow 
     interrupts();                         // sei();
-    sleep_cpu();
+    
+    sleep_cpu();                          // good night
+    
     sleep_disable();
     wdtDisable();                         
-    power_all_enable();
+    power_all_enable();                   // clr PRR reg bits
     ADCSRA |= (1<<ADEN);                  // ADC enable
 
-    return wdtTime[durat]/10;
+    return wdtTime[durat]/10;             // not valid if ext interrupt awake
 }
 
 void letalSleep()
@@ -208,15 +228,98 @@ void letalSleep()
     cli();
     sleep_enable();
     sleep_bod_disable();
-    sleep_cpu();            // neither interrupt nor wdt -> reset only to awake cpu
+    sleep_cpu();                          // neither interrupt nor wdt -> reset only to awake cpu
 }
 
-void setup() {
-    
+void hardPwrDown()
+{
+    pinMode(LED,INPUT);
+    pinMode(REED,INPUT);
+#ifdef NRF
+    nrf24l01PwrDown();
+#endif
+/*    pinMode(CSN_PIN,INPUT);     // 340 to 400 uA
+    pinMode(CLK_PIN,INPUT);
+    pinMode(CE_PIN,INPUT);*/
+
+/* 3 pins low                        290 uA */
+
+/*    
+    digitalWrite(CSN_PIN,HIGH);     // 290 to 305 uA
+    digitalWrite(CLK_PIN,LOW);
+    digitalWrite(CE_PIN,LOW);
+
+    digitalWrite(CSN_PIN,LOW);     // 111 uA
+    digitalWrite(CLK_PIN,HIGH);
+    digitalWrite(CE_PIN,LOW);
+
+    digitalWrite(CSN_PIN,HIGH);     // 302 to 305 uA
+    digitalWrite(CLK_PIN,HIGH);
+    digitalWrite(CE_PIN,LOW);
+
+    digitalWrite(CSN_PIN,LOW);     // 497 to 505 uA
+    digitalWrite(CLK_PIN,LOW);
+    digitalWrite(CE_PIN,HIGH);
+
+    digitalWrite(CSN_PIN,HIGH);     // 444 to 446 uA
+    digitalWrite(CLK_PIN,LOW);
+    digitalWrite(CE_PIN,HIGH);
+
+    digitalWrite(CSN_PIN,LOW);     // 503 to 561 uA
+    digitalWrite(CLK_PIN,HIGH);
+    digitalWrite(CE_PIN,HIGH);
+
+    digitalWrite(CSN_PIN,HIGH);     // 490 to 494 uA
+    digitalWrite(CLK_PIN,HIGH);
+    digitalWrite(CE_PIN,HIGH);
+*/
+}    
+
+#ifdef NRF
+void nrf24l01PwrDown()
+{
+    SPI.beginTransaction(SPISettings(4000000,MSBFIRST,SPI_MODE0));  // init SPI
+    SPI.begin();
+
+    digitalWrite(CE_PIN,LOW);
+    pinMode(CE_PIN,OUTPUT);
+    digitalWrite(CSN_PIN,LOW);                                      // enable device access 
+    pinMode(CSN_PIN,OUTPUT);
+
+/* NRF CONF REG BITS */    
+#define MASK_RX_DR_BIT 0x40
+#define MASK_TX_DS_BIT 0x20
+#define MASK_MAX_RT_BIT 0x10
+#define EN_CRC_BIT  0x08
+#define CRCO_BIT    0x04
+#define PWR_UP_BIT  0x02
+#define PRIM_RX_BIT 0x01
+     
+#define CONFREG (0x00 & ~(MASK_RX_DR_BIT) & ~(MASK_TX_DS_BIT) & ~(MASK_MAX_RT_BIT) | EN_CRC_BIT & ~(CRCO_BIT) & ~(PWR_UP_BIT) | PRIM_RX_BIT )
+    byte conf=CONFREG;                                                // PWR_UP_BIT = 0
+        
+#define W_REGISTER    0x20                                            // write register command
+    SPI.transfer(W_REGISTER);                                         // config register is 0x00
+    SPI.transfer(conf);
+    digitalWrite(CSN_PIN,HIGH);
+
+    SPI.end();                                                        // disable SPI and pins
+    pinMode(MOSI_PIN,INPUT);                               
+
+    digitalWrite(MISO_PIN,HIGH);                                      // SPI MISO pin
+    digitalWrite(CLK_PIN,LOW);                                        // SPI CK pin
+}
+#endif NRF
+
+void setup()
+{    
     Serial.begin(115200);
-    Serial.println("test sleep ready");
+    Serial.println("test sleep ready ");
+    Serial.print(getVolts());Serial.println("V");
    
-  pinMode(REED,INPUT_PULLUP); // must stay pullup when reed mode active
+    hardPwrDown();
+    
+    pinMode(REED,INPUT_PULLUP); // must stay pullup when reed mode active
     
     nbBlk=5;
 }
@@ -224,21 +327,22 @@ void setup() {
 void loop() 
 {
 
-  /* 5 blinks au reset puis 8 sec sleep suivi d'1 blink jusqu'à contact du reed
-   * à chaque contact changement de mode avec 16 blinks de tempo pour retirer l'aimant
+  /* 5 blinks at reset then 8 sec sleep followed by 1 blink until reed contact
+   * at every reed contact mode change with 16 blinks tempo to allow magnet removal
+   * (when timed sleep mode, reed contact is only checked at awake)
    */
 
-  
   bool reed=digitalRead(REED);Serial.println(reed);delay(1);
-  if(!reed){ nbBlk=3;}                 // if reed on (pullup low) chge mode
+  if(!reed){ nbBlk=3;}                    // if reed on (pullup low) chge mode
 
   
   switch(nbBlk){
-    case 1:sleepPwrDown(T8000);break;     // >3uA     
+    case 1:sleepPwrDown(T8000);break;     // <4uA timed sleep (power increase when pullup low) (~5uA with NRF ; @3V 2,4uA)
     case 3:
-      for(int i=0;i<10;i++){blk(2);}      // tempo armement pour retirer l'aimant
-      sleepPwrDown(0);                    // >1uA
-      for(int i=0;i<8;i++){blk(2);}       // tempo désarmement pour retirer l'aimant      
+      for(int i=0;i<10;i++){blk(2);}      // magnet removal tempo
+      sleepPwrDown(0);                    // <2uA ext interrupt sleep (no timer) (power increase when pullup low) 
+                                          // (~3uA with NRF ; @3V too small to be mesured)
+      for(int i=0;i<8;i++){blk(2);}       // magnet removal tempo
       break;              
     default:break;
   }
